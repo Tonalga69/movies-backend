27974201153d1f79e7c997beea90e0446ba17a72
SamuelR69 <114710082+Tonalga69@users.noreply.github.com>
2024-09-05 17:34:27 -0600

    AuthService created

--- src/main/java/com/example/demo/auth/controllers/AuthController.java
+++ src/main/java/com/example/demo/auth/controllers/AuthController.java
@@ -3,6 +3,7 @@ package com.example.demo.auth.controllers;
 
 import com.example.demo.auth.entities.LoginRequest;
 import com.example.demo.auth.entities.LoginResponse;
+import com.example.demo.auth.service.AuthService;
 import com.example.demo.config.jwt.JWTIssuer;
 import com.example.demo.user.entities.User;
 import com.example.demo.user.repositories.UserRepository;
@@ -16,29 +17,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/auth")
-@RequiredArgsConstructor
 public class AuthController {
 
 
 
-    private final JWTIssuer jwtIssuer;
+    private final AuthService authService;
 
-    @Autowired
-    private UserRepository userRepository;
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+
     @PostMapping(path = "/login", consumes = "application/json", produces = "application/json")
     private ResponseEntity<LoginResponse> login(@RequestBody @Validated LoginRequest loginRequest) {
-        final User user = userRepository.findByEmail(loginRequest.getEmail()).orElse(null);
-        if (user==null){
-            return ResponseEntity.badRequest().body(new LoginResponse(null,"User not found"));
+        final Optional<String> token = authService.login(loginRequest.getEmail(), loginRequest.getPassword());
+        if (token.isEmpty()) {
+            return ResponseEntity.badRequest().body(new LoginResponse(null, "User not found"));
         }
-        if (!user.getPassword().equals(loginRequest.getPassword())){
-            return ResponseEntity.badRequest().body(new LoginResponse(null,"User not found"));
-        }
-        final String token = jwtIssuer.issueToken(user.getId(), user.getEmail(), List.of(user.getUserRole()));
-        return ResponseEntity.ok(new LoginResponse(token, "Login Successful"));
+        return ResponseEntity.ok(new LoginResponse(token.get(), "Login Successful"));
     }
 
     @PostMapping("/logout")
